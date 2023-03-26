cf144958425d7b9a568eedec3ac6d5465a8a2567
marat <marat.forsky@gmail.com>
2023-03-27 00:32:31 +0300

    hw done

---
+++ src/pro/sky/java/course1/lessonObjects2003/Author.java
@@ -0,0 +1,16 @@
+package pro.sky.java.course1.lessonObjects2003;
+
+public class Author {
+
+    private String authorsName;
+
+    public Author (String authorsName){
+        this.authorsName = authorsName;
+    }
+
+
+    public String getAuthorsName(){
+        return this.authorsName;
+
+    }
+}
--- src/pro/sky/java/course1/lessonObjects2003/Main.java
+++ src/pro/sky/java/course1/lessonObjects2003/Main.java
@@ -1,7 +1,27 @@
 package pro.sky.java.course1.lessonObjects2003;
 
+import com.sun.source.doctree.AuthorTree;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author FirstAuthor = new Author(" Автор первой книги. ");
+        Book FirstBook = new Book("Первая книга. ", 2000);
+
+
+        System.out.println(FirstBook.getBook() + FirstBook.getPublicationYear() + FirstAuthor.getAuthorsName());
+
+        Author SecondAuthor = new Author(" Автор второй книги. ");
+        Book SecondBook = new Book("Вторая книга. ", 2022);
+
+
+        System.out.println(SecondBook.getBook() + SecondBook.getPublicationYear() + SecondAuthor.getAuthorsName());
+
+        FirstBook.setPublicationYear(1995);
+        System.out.println(FirstBook.getBook() + FirstBook.getPublicationYear() + FirstAuthor.getAuthorsName());
+
+
+
+
     }
 }
