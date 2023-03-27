2e9bb2966bce1dc5ddaf3575ac2b6ca706cf03c9
marat <marat.forsky@gmail.com>
2023-03-27 23:49:40 +0300

    fixed

--- src/pro/sky/java/course1/lessonObjects2003/Book.java
+++ src/pro/sky/java/course1/lessonObjects2003/Book.java
@@ -2,16 +2,19 @@ package pro.sky.java.course1.lessonObjects2003;
 
 public class Book {
 
-    private String book;
+    public Author author;
 
-  // public String Author;
+    private String book;
 
     private int publicationYear;
 
-    public Book  (String book,int publicationYear) {
+    public Book  (String book,Author author, int publicationYear ) {
+
         this.book =book;
+        this.author = author;
         this.publicationYear = publicationYear;
     }
+
 
     public String getBook(){
         return this.book;
@@ -21,9 +24,13 @@ public class Book {
         return this.publicationYear;
     }
 
+
+    public Author getAuthor(){
+        return this.author;
+    }
+
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
     }
-
 
     }
